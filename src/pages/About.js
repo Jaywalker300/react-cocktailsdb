c82c55bc9740f4433529b9a6fd1a64dfe2571fe6
jaywalker <chiketravis300@gmail.com>
2023-02-13 01:10:33 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim voluptatem quos voluptates? Hic magnam, nemo in nobis tenetur necessitatibus incidunt doloremque quaerat architecto minima, officiis quo pariatur blanditiis non, natus ipsam voluptatibus possimus. Eum quia unde, labore quod a repellendus.</p>
+    </section>
+  )
+}
+
+export default About
